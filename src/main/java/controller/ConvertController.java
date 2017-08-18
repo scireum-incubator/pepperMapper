1636a08b67f067b2a63a40a0007e40876239a8d9
meg <meg@scireum.de>
2017-08-18 15:26:41 +0200

    Updates sirius librarys

--- src/main/java/controller/ConvertController.java
+++ src/main/java/controller/ConvertController.java
@@ -8,9 +8,8 @@ import sirius.web.controller.BasicController;
 import sirius.web.controller.Controller;
 import sirius.web.controller.DefaultRoute;
 import sirius.web.controller.Routed;
+import sirius.web.data.ExcelExport;
 import sirius.web.http.WebContext;
-import sirius.web.templates.ExcelExport;
-
 
 @Register(classes = Controller.class)
 public class ConvertController extends BasicController {
@@ -39,7 +38,12 @@ public class ConvertController extends BasicController {
             JSONObject fields = entity.getJSONObject("properties");
             fields.forEach((key2, value2) -> {
                 JSONObject field = fields.getJSONObject(key2);
-                excelExport.addRow(key2, field.get("type"), field.get("index"), field.get("store"), field.get("include_in_all"), field.get("format"));
+                excelExport.addRow(key2,
+                                   field.get("type"),
+                                   field.get("index"),
+                                   field.get("store"),
+                                   field.get("include_in_all"),
+                                   field.get("format"));
             });
             excelExport.addRow();
         });
